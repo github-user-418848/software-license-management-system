84f4a01b481ed5fe9267e069868ad2b0f6d89a3a
github-user-418848 <x1cjlyjr@duck.com>
2023-07-18 19:55:15 +0800

    refactored code and removed token

--- users/serializers.py
+++ users/serializers.py
@@ -4,13 +4,13 @@ from .models import CustomUser
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = ['email', 'username', 'first_name', 'last_name', 'roletype', 'is_active', 'date_joined']
-        read_only_fields = ['token', 'roletype', 'is_staff', 'date_joined']
+        fields = ['image', 'email', 'username', 'first_name', 'last_name', 'roletype', 'is_active', 'date_joined']
+        read_only_fields = ['roletype', 'is_staff', 'date_joined']
 
 class SuperUserAdminSerializer(UserSerializer):
     class Meta(UserSerializer.Meta):
         fields = ['image', 'email', 'username', 'first_name', 'last_name', 'roletype', 'is_active', 'date_joined']
-        # read_only_fields = ['token', 'roletype', 'is_staff', 'date_joined']
+        read_only_fields = ['date_joined']
         
         
 class UserRegistrationSerializer(serializers.ModelSerializer):
