e89b8d02f1c7c51fe2740f6167f929a5e4df9f13
github-user-418848 <x1cjlyjr@duck.com>
2023-07-18 19:59:26 +0800

    set a 'users' basepath for UserViewSet

--- users/urls.py
+++ users/urls.py
@@ -1,16 +1,30 @@
-from django.urls import include, path
+# from django.urls import include, path
+# from rest_framework.routers import DefaultRouter
+
+# from django.urls import path
+# from .views import PrivilegedUserViewSet, LoginView, LogoutView
+
+# router = DefaultRouter()
+# router.register('users', PrivilegedUserViewSet, basename='users')
+
+# urlpatterns = [
+#     path('', PrivilegedUserViewSet.as_view({'get': 'list'}), name='user.list'),
+#     path('<int:id>/', PrivilegedUserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='user.detail'),
+#     path('register/', PrivilegedUserViewSet.as_view({'post': 'create'}), name='user.register'),
+#     path('login/', LoginView.as_view(), name='user.login'),
+#     path('logout/', LogoutView.as_view(), name='user.logout'),
+# ]
+
 from rest_framework.routers import DefaultRouter
 
-from .views import UserViewSet, LoginView, LogoutView, ChangePasswordView
+from django.urls import path
+from .views import PrivilegedUserViewSet, LoginView, LogoutView
 
 router = DefaultRouter()
-router.register('users', UserViewSet, basename='users')
+router.register('users', PrivilegedUserViewSet, basename='users')
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('users/<str:token>/', UserViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='user.detail'),
-    path('register/', UserViewSet.as_view({'post': 'register'}), name='user.register'),
+    *router.urls,
     path('login/', LoginView.as_view(), name='user.login'),
     path('logout/', LogoutView.as_view(), name='user.logout'),
-    path('change-password/', ChangePasswordView.as_view(), name='user.change_password'),
 ]
